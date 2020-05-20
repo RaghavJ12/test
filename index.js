98bb7e3c275a2fdf70b4fb671f366fabf5a7889a
RaghavJ12 <50922572+RaghavJ12@users.noreply.github.com>
2020-05-20 18:24:27 +0530

    Add files via upload
    newer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const firebase=require("firebase");
+const firebase=require("firebase/app");
+require("firebase/auth");
 //const admin=require("firebase-admin");
 const abc=require("watchmate-c5051-firebase-adminsdk-tgjns-e412e98a25.json");
 (function(){
